fa7f88de7537b48456000b8f0c87f836350b14c1
Eskeadmas Tesfaye <teskeadmas@gmail.com>
2026-02-18 08:59:06 +0300

    Fix the report status

--- script.js
+++ script.js
@@ -42,8 +42,8 @@ const login = async (username, password) => {
     const response = await fetch(`${API_BASE_URL}/users/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password }),
       credentials: "include",
+      body: JSON.stringify({ username, password }),
     });
 
     const result = await response.json();
